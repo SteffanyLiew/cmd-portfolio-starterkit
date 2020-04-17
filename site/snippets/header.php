fa6fc5502c4b8c112fedcf92adf1df6f8fb957d3
SteffanyLiew <0935507@hr.nl>
2020-04-17 22:38:26 +0200

    header

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -23,7 +23,7 @@
     <nav class="navbar navbar-expand-lg navbar-light" >
       <a class="navbar-brand" href="<?= $site->url() ?>">
         <img src="<?= $site->image()->url() ?>" width="48px" height="48px" alt="<?= $site->image()->alt() ?>" class="rounded-circle"/>
-      
+       
       </a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -35,7 +35,6 @@
             <a href="<?= $item->url(); ?>" class="nav-link"><?= $item->title(); ?></a>
           </li>
           <?php endforeach ?>
-        
           <li class="nav-item">
             <a class="nav-link" href="#social">Contact me</a>
           </li>
--- site/templates/home.php
+++ site/templates/home.php
@@ -1,11 +1,12 @@
 <?php snippet('header') ?>
 
+
 <main>
   <div class="row align-items-center">
     <div class="col-12 col-lg-6">
       <h1><?= $page->introductiontitle() ?></h1>
       <p class="lead"><?= $page->introductiontext() ?></p>
-      <a href="<?= $site->page('projects') ?>" class="btn btn-primary" role="button" aria-pressed="true">View my work</a>
+     
       <a href="<?= $site->page('about') ?>" class="btn btn-outline-primary" role="button" aria-pressed="true">More about me</a>
     </div>
     <div class="col-12 col-lg-6">
